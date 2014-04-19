b8325f3614bb71f5f0ad814eda550c64b09f8ce2
Daryl Turner <daryl@daryls-mbp.home>
2014-04-19 11:27:58 +0100

    initial skeleton code

--- fortifuncs/fortifuncs.py
+++ fortifuncs/fortifuncs.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python
+from getpass import getpass
+import os
+
+
+class FortiGate()
+	ip = '192.168.1.1'
+	user = None
+	passw = None
+
+	def __init__(self, ip, user=None, passw=None):
+		self.ip = ip
+		if passw is None:
+			self.passw = getpass()
+		if user is None:
+			self.user = os.getlogin()
+
+	
+	def print(self):
+		print(ip, user, passw)
