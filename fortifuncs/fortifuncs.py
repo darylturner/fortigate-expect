3106e38497e3743f7df537d925914aa7fb845847
Daryl Turner <daryl@daryls-mbp.home>
2014-04-19 16:05:41 +0100

    added interface method. removed redundant code.

--- fortifuncs/fortifuncs.py
+++ fortifuncs/fortifuncs.py
@@ -6,13 +6,12 @@ import os
 
 class FortiGate():
     '''Fancy wrapper to pexpect for FortiGate firewalls'''
-    ip = None
-    user = None
-    passw = None
     connected = False
 
-    def __init__(self, ip, user=None, passw=None):
+    def __init__(self, ip, user=None, passw=None, vdom=None):
         self.ip = ip
+        self.vdom = vdom
+
         if passw is None:
             self.passw = getpass()
         else:
@@ -24,19 +23,27 @@ class FortiGate():
             self.user = user
 
     def opts(self):
-        print(self.ip, self.user, self.passw)
+        print(self.ip, self.user, self.passw, self.vdom)
 
     def connect(self):
-        print("pexpect spawn code goes here")
         print("ssh to {}@{}".format(self.user, self.ip))
-        self.connected = True
+        self.client = pexpect.spawn('ssh {}@{}'.format(self.user, self.ip))
+        ans = self.client.expect([pexpect.TIMEOUT, '# '], timeout=30)
+        if ans == 0:
+            print("ssh has timed out: {}, {}".format(self.client.before, self.client.after))
+        elif ans == 1:
+            print("ssh connected")
+            self.connected = True
 
     def disconnect(self):
         print("pexpect close goes here")
         self.connected = False
 
-    def add_policy(self, srcintf, dstintf, srcaddr, dstaddr, action='permit',
-                   schedule='always', nat, natpool):
+    def add_interface(self, name, vlan, phy, ip, vdom):
+        pass
+
+    def add_policy(self, srcintf, dstintf, srcaddr, dstaddr, nat, natpool,
+                   action='permit', schedule='always'):
         pass
 
     def add_address(self, name, subnet):
